6e3cc44603d449c3a1c4cb71c299615f7a600597
Calum Gilchrist <calum@homelinen.org>
2012-12-15 05:44:25 +0000

    Changed Keyboard to Holding, Started Vec Implementation

--- pirates.rb
+++ pirates.rb
@@ -1,14 +1,18 @@
 require "chingu"
+
 include Gosu
 include Chingu
+
+require_relative "vector.rb"
 
 class Game < Chingu::Window
   def initialize
     super
     @player = Player.create 
     @player.input = {
-        :left => :steer_left, 
-        :right => :steer_right,
+        :holding_left => :steer_left, 
+        :holding_right => :steer_right,
+        :up => :move_forward,
         :space => :fire
     }
   end    
@@ -18,19 +22,25 @@ class Player < Chingu::GameObject
 
     def initialize(options = {})
         super(options.merge( :image => Image["pirateShip.png"] ))
+
+        # Duplication in data, sadly
+        @vector = Vector.new(@x, @y)
     end
 
     def move_forward
        # Move in the vector you are pointing 
-        
+       temp_vector = Vector.new(1, @y).normalise 
+       temp_vector.rotate! 90
+
+       puts temp_vector
     end
 
     def steer_right
-        @angle += 1
+        @angle += 2
     end
 
     def steer_left
-        @angle -= 1
+        @angle -= 2
     end
 
     def fire
