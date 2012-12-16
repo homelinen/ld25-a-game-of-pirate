8103ed19b41a4acaa4cb1f033dbd199f7cc41e1e
Calum Gilchrist <calum@homelinen.org>
2012-12-16 19:39:41 +0000

    Moved Beach into Island Tiles, Added a House Class

--- lib/beach.rb
+++
@@ -1,9 +0,0 @@
-require "chingu"
-
-class Beach < Chingu::GameObject
-    trait :bounding_box
-
-    def initialize(options = {})
-        super(options)
-    end
-end
--- lib/island_tiles.rb
+++ lib/island_tiles.rb
@@ -6,3 +6,26 @@ class Grass < Chingu::GameObject
         super options
     end
 end
+
+class House < GameObject
+    trait :animation
+    trait :bounding_box
+    
+    def initialize(options = {})
+        super(options)
+
+        @resident_count = options[:resident_count]
+    end
+
+    def update
+        @image = self.animate.next if self.animation
+    end
+end
+
+class Beach < Chingu::GameObject
+    trait :bounding_box
+
+    def initialize(options = {})
+        super(options)
+    end
+end
