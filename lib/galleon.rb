b98867476698230c9cd2ba344897cc8de53ac0ed
Calum Gilchrist <calum@homelinen.org>
2012-12-16 11:03:58 +0000

    Added a Galleon Class with Very Simple AI

---
+++ lib/galleon.rb
@@ -0,0 +1,39 @@
+require_relative "ship"
+
+class Galleon < Ship
+
+    traits :collision_detection, :bounding_box
+    
+    def initialize(options = {})
+        super(options.merge(:image => Image["galleon.png"]))
+    end
+
+    def update
+
+        # plot course
+
+        if (@x >= 0 && @x < $window.width) && (@y >= 0 && @y < $window.height)
+            # Flip a coin
+            direction = rand(10)
+
+            # Randomly pick  a new direction, with a small chance of success
+            if direction == 6
+                @angle -= 5
+            elsif direction == 3
+                @angle += 5
+            end
+        else
+            # Turn around
+            @angle += 180
+        end
+
+        if rand(200) == 0
+            fire
+        end
+
+        move_forward
+        
+        # If see enemy, engage
+    end
+
+end
